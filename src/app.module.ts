5c81d36d8223d416fd7355934a8c9fc475d343ae
Richie <20235894@esen.edu.sv>
2025-07-07 19:14:21 -0600

    feat: integrate caching and PostgreSQL with TypeORM
    - Added @nestjs/cache-manager and related dependencies for caching functionality.
    - Configured CacheModule with Redis as the store in CacheConfigService.
    - Implemented Product entity with TypeORM for PostgreSQL database.
    - Created ProductModule, ProductController, and ProductService for managing products.
    - Implemented caching logic in ProductService to cache product data.
    - Added unit tests for CacheService, ProductController, and ProductService.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './product/product-entity/product-entity';
+import { ProductModule } from './product/product.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { CacheConfigService } from './cache/cache.service';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'cache_error1',
+      entities: [Product],
+      synchronize: true,
+    }),
+    CacheModule.registerAsync({
+      useClass: CacheConfigService,
+    }),
+    ProductModule,
+  ],
+  providers: [CacheConfigService],
 })
 export class AppModule {}
